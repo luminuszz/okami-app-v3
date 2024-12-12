845121ac3b08a1f88eb47992babe8d878ad2a5e5
luminuszz <luminuszz43@gmail.com>
2024-12-12 12:44:21 -0300

    feat deep link

--- src/components/home/continuous-reading-section.tsx
+++ src/components/home/continuous-reading-section.tsx
@@ -26,7 +26,6 @@ export function ContinuousReadingSection() {
   const handlePushToUrl = useGoToWorkUrlAction();
 
   const openProfile = useSetAtom(toggleProfileDrawerActionAtom);
-
   const lastSelectedWorkStorage = useAsyncStorage(STORAGE_KEYS.LAST_WORK_CLICKED);
 
   const [currentWorkId, setCurrentWork] = useState<string | null>();
@@ -53,8 +52,6 @@ export function ContinuousReadingSection() {
       });
     }, [lastSelectedWorkStorage]),
   );
-
-  console.log(currentWorkId);
 
   if (isLoading || !work) {
     return (
